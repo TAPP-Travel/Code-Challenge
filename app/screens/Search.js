7e3db7afe89e41abd0fe7cbe465148f645956808
Brent <bingr001@gmail.com>
2018-10-05 08:16:23 -0700

    added more changes to autocomplete

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
-
+import Autocomplete from '../components/autocomplete';
 
 export default ({ history }) => (
-    <View>
-        <Text> This is Search page</Text>
-        <Button title="Back" onPress={() => history.push("/")} />
-        <Button title="Add New Place" onPress={() => history.push("/place")} />
-    </View>
+  <View style={{ flex: 1, padding: 20, justifyContent: "space-between" }}>
+        <Text
+            style={{
+                fontSize: 18,
+                textAlign: 'center',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        > 
+        Search
+        </Text>
+
+    <Autocomplete/>
+
+    <Button title="Back" onPress={() => history.push("/")} />
+    <Button title="Add New Place" onPress={() => history.push("/place")} />
+  </View>
 );
 
--- app/screens/bookmarks.js
+++ app/screens/bookmarks.js
@@ -3,11 +3,20 @@ import {View, Text, Button} from 'react-native';
 
 
 export default ({ history }) => (
-  <View>
-    <Text>Bookmark 1</Text>
-    <Text>Bookmark 2</Text>
-    <Text> This is the Bookmarks page</Text>
-    <Button title="Add New Place" onPress={() => history.push("/search")} />
+  <View style={{ flex: 1, padding: 20, justifyContent: "space-between" }}>
+    <Text
+      style={{
+        fontSize: 18,
+        textAlign: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {"\n"}  Bookmarks
+      </Text>
+    <Button title="Add New Place" onPress={() => history.push("/search")} 
+    />
   </View>
 );
 
