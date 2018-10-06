a67cd9ee58c24827f2224ba3e835b0aa956110dc
Brent <bingr001@gmail.com>
2018-10-06 14:39:59 -0700

    styled places screen

--- app/components/placePhoto.js
+++ app/components/placePhoto.js
@@ -4,17 +4,9 @@ import {AppRegistry, View, ImageBackground} from "react-native";
 export default class DisplayAnImage extends Component {
     render() {
 
-        return (
-            <View style={{backgroundColor: '#f0f', flex:1 }}>
-                
-                <ImageBackground
-                    style={{ width: 400, height: 400, }}
-                    resizeMode="contain"
-                    source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
-                />
-                
-            </View>
-        );
+        return <View style={{  flex:1 }}>
+            <ImageBackground style={{ width: 400, height: 400 }} resizeMode="contain" source={{ uri: "https://www.discoverlosangeles.com/sites/default/files/styles/featured_item_detail/public/poi_images/los_angeles_county_museum_of_art_lacma/h_2000crmlalacma-urban-lights-0_7b09047e-5056-a36f-23589df0c943a4dc.jpg" }} />
+          </View>;
     }
 }
 AppRegistry.registerComponent("DisplayAnImage", () => DisplayAnImage);
--- app/screens/Place.js
+++ app/screens/Place.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text} from 'react-native';
+
 import DisplayAnImage from '../components/placePhoto';
+import { Card, Button } from "react-native-elements";
 
 //Eventually this would have the logic to pull in the place detail and photo from what is selected from
 //the list on the Search page
-export default ({history}) => (
-  <View style={{ flex: 1, backgroundColor: '#0f5e9b'}}>
-    <DisplayAnImage />
+export default ({ history }) => (
+  <View style={{ flex: 1 }}>
+    <DisplayAnImage style={{ flex: 1 }} />
 
-    
-    <Button title="Back" onPress={() => history.push("/search")} />
+    <Card title="LACMA" 
+    containerStyle={{ flex: 1, 
+             paddingBottom: 100 ,
+             borderTopLeftRadius: 10,
+             borderTopRightRadius: 10
+             
+             } }>
+      <Text style={{ marginBottom: 10 }}>
+        The idea with React Native Elements is more about component structure
+        than actual design.
+      </Text>
+      <View/>
+      <View/>
+      <Button
+        backgroundColor="rgba(4,4,206,1)"
+        buttonStyle={{
+          borderRadius: 10,
+          marginLeft: 0,
+          marginRight: 0,
+          marginBottom: 100
+        }}
+        title="Bookmark" onPress={() => history.push("/")}
+      />
+    </Card>
+
   </View>
 );
 
