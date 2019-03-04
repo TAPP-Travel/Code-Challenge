ca5c33b0f5813b820536af674711c50051c0efdc
Walter shub <waltershub@Walters-MBP-2.fios-router.home>
2019-03-04 00:20:58 -0500

    detail picture

--- app/places_app/src/screens/placeDetail.js
+++ app/places_app/src/screens/placeDetail.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { verticalScale } from 'react-native-size-matters';
+import { Image, Text, View } from 'react-native';
+import GoogleStaticMap from 'react-native-google-static-map';
+import { scale, verticalScale } from 'react-native-size-matters';
+import googleApiKey from '../../config';
 import MapContextToProps from '../context/mapContextToProps';
 
 class PlaceDetail extends React.Component {
@@ -10,6 +12,8 @@ class PlaceDetail extends React.Component {
     this.props.navigation.navigate('PlaceDetail');
   };
   render() {
+    const photoReference = this.props.context.selectedPlace.details.photos[0].photo_reference;
+    console.log(photoReference);
     return (
       <View
         style={{
@@ -19,6 +23,19 @@ class PlaceDetail extends React.Component {
           marginTop: verticalScale(20),
         }}>
         <Text>Detail</Text>
+        <Image
+          style={{ height: verticalScale(100), width: scale(100) }}
+          source={{
+            uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${googleApiKey}`,
+          }}
+        />
+        <GoogleStaticMap
+          latitude={'32.064171'}
+          longitude={'34.7748068'}
+          zoom={14}
+          size={{ width: '100%', height: verticalScale(100) }}
+          apiKey={googleApiKey}
+        />
       </View>
     );
   }
