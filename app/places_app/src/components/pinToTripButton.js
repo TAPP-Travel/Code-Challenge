72851eb528ac05705e4512afd5044d576cd7036a
Walter shub <waltershub@Walters-MacBook-Pro-2.local>
2019-03-04 23:39:45 -0500

    added pin to trip button

---
+++ app/places_app/src/components/pinToTripButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { moderateScale, verticalScale } from 'react-native-size-matters';
+const check = require('../../assets/checkmarkIcon.png');
+
+const PinToTripButton = ({ pinToTrip, isPinned }) => (
+  <TouchableOpacity onPress={pinToTrip}>
+    <View
+      style={{
+        backgroundColor: !isPinned ? '#1313AF' : '#00FF1C',
+
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: verticalScale(30),
+        borderRadius: moderateScale(30),
+        marginVertical: verticalScale(10),
+      }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        {isPinned && <Image source={check} />}
+        <Text style={{ color: isPinned ? '#000' : '#FFF' }}>
+          {isPinned ? 'Pinned to Trip' : 'Pin to Trip'}
+        </Text>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+
+export default PinToTripButton;
--- app/places_app/src/screens/placeDetail.js
+++ app/places_app/src/screens/placeDetail.js
@@ -4,17 +4,20 @@ import GoogleStaticMap from 'react-native-google-static-map';
 import { scale, verticalScale } from 'react-native-size-matters';
 import googleApiKey from '../../config';
 import DetailHeader from '../components/detailHeader';
+import PinToTripButton from '../components/pinToTripButton';
 import MapContextToProps from '../context/mapContextToProps';
 class PlaceDetail extends React.Component {
-  goToDetail = async selectedPlace => {
-    const { selectPlace } = this.props.context;
-    await selectPlace(selectedPlace);
-    this.props.navigation.navigate('PlaceDetail');
+  state = {
+    isPinned: false,
+  };
+
+  pinToTrip = () => {
+    this.setState({ isPinned: true });
   };
   render() {
     const photoReference = this.props.context.selectedPlace.details.photos[0].photo_reference;
     const { lat, lng } = this.props.context.selectedPlace.details.geometry.location;
-    const {  rating } = this.props.context.selectedPlace.details;
+    const { rating } = this.props.context.selectedPlace.details;
     console.log(photoReference);
     const detailImage = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${googleApiKey}`;
     const {
@@ -30,6 +33,7 @@ class PlaceDetail extends React.Component {
         <StatusBar hidden />
         <DetailHeader image={detailImage} name={main_text} city={secondary_text} rating={rating} />
         <View>
+          <PinToTripButton isPinned={this.state.isPinned} pinToTrip={this.pinToTrip} />
           <GoogleStaticMap
             latitude={lat.toString()}
             longitude={lng.toString()}
