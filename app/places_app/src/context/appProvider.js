9ccca4047354670b6246da9a24408d7b1bde6a45
Walter shub <waltershub@Walters-MacBook-Pro-2.local>
2019-03-06 00:27:40 -0500

    added bookmark persistance

--- app/places_app/src/context/appProvider.js
+++ app/places_app/src/context/appProvider.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import Context from './context';
 
 class AppProvider extends React.Component {
@@ -6,17 +7,26 @@ class AppProvider extends React.Component {
     bookmarks: [],
     selectedPlace: {},
   };
+  //checks for stored book marks
+  async componentDidMount() {
+    const bookmarks = await AsyncStorage.getItem('bookmarks');
+    if (bookmarks) this.setState({ bookmarks: JSON.parse(bookmarks) });
+    AsyncStorage.setItem('bookmarks', JSON.stringify(this.state.bookmarks));
+    // AsyncStorage.removeItem('bookmarks');
+  }
 
   selectPlace = async selectedPlace => await this.setState({ selectedPlace });
 
-  addBookmark = place => {
+  //adds and persists bookmarks
+  addBookmark = async place => {
     const bookmarks = this.state.bookmarks.concat(place);
-    this.setState({ bookmarks });
+    await this.setState({ bookmarks });
+    AsyncStorage.setItem('bookmarks', JSON.stringify(this.state.bookmarks));
   };
   render() {
     return (
       <Context.Provider
-        value={{ ...this.state, selectPlace: this.selectPlace, addBookmark: this.addBookmark  }}>
+        value={{ ...this.state, selectPlace: this.selectPlace, addBookmark: this.addBookmark }}>
         {this.props.children}
       </Context.Provider>
     );
